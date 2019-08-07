b8ba5a458eee9670426499cdbea227fd633de01a
trinamae12 <maetrina12@gmail.com>
2019-08-07 23:22:55 +0800

    Add top nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsListComponent
+    ContactsListComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,

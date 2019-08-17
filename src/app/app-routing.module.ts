95e9466a63580d4445fe6133e004eed2398b31b4
trinamae12 <maetrina12@gmail.com>
2019-08-17 20:41:54 +0800

    Fix route bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: ContactsListComponent
   },
   {
-    path: 'products/:productId',
+    path: 'contacts/:contactId',
     component: ContactDetailsComponent
   }
 ];
